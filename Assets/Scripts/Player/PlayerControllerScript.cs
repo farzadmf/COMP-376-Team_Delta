d58040f81cc4855e860496d762f01059e9437f29
Radu <radu.saghin94@gmail.com>
2016-09-26 16:03:14 -0400

    Put a double jump for the dude.

---
+++ Assets/Scripts/Player/PlayerControllerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour {
+	public float Speed = 0f;
+	private float movex = 0f;
+	private float movey = 0f;
+	private bool touchingGround;
+	public float jumpHeight;
+	private Rigidbody2D rigidBody;
+	private bool canDoubleJump;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+
+	void Update() {
+		movex = Input.GetAxis ("Horizontal");
+
+		if (Input.GetKeyDown("space")) {
+			if (GetComponent<PlayerScript> ().getGrounded () == true) {
+				rigidBody.velocity = new Vector2 (rigidBody.velocity.x,0);
+				rigidBody.AddForce (new Vector2 (0, jumpHeight));
+				canDoubleJump = true;
+			} else {
+				if (canDoubleJump == true) {
+					canDoubleJump = false;
+					rigidBody.velocity = new Vector2(rigidBody.velocity.x,0);
+					rigidBody.AddForce (new Vector2 (0, jumpHeight));
+				}
+			}
+
+		}
+		//movey = Input.GetAxis ("Vertical"); we'll use this later for ladders
+		rigidBody.velocity = new Vector2 (movex * Speed,rigidBody.velocity.y);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	private bool grounded;
+	public Transform groundCheckTransform;
+	public LayerMask groundCheckLayerMask;
+	private float radius = 0.2f;
+
+	void Start() {
+		
+	}
+	public bool getGrounded() {
+		return grounded;
+	}
+
+	void Update() {
+		updateGroundedStatus ();
+	}
+	void updateGroundedStatus() {
+		if (Physics2D.OverlapCircle (groundCheckTransform.position, radius, groundCheckLayerMask)) {
+			grounded = true;
+		} else
+			grounded = false;
+
+	}
+}
