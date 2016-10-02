f1a917497c69c11f853a5c0b3abc153d060d1ef1
Radu <radu.saghin94@gmail.com>
2016-10-01 20:10:28 -0400

    MOAR magic. To use attacks, press left and right click. Use the editor to change the player's level to try out different magic attacks. Only level 1,2,3 have been implemented. Of course these are just numbers, we'll decide later when we want to unlock these super awesome powers.

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -21,15 +21,29 @@ public class PlayerScript : MonoBehaviour {
 	void Update() {
 		updateGroundedStatus ();
 		if (Input.GetMouseButtonDown (1)) {
-			shootFireball ();
+			if (level == 1 || level == 2)
+				shootFireball ();
+			else if (level == 3)
+				fireBurst ();
 		}
 	}
 
-
+	void fireBurst() {
+		GameObject g = (GameObject)Instantiate (Resources.Load ("FireBurst"));
+		if (transform.eulerAngles.y == 0)
+			g.transform.position = new Vector3 (transform.position.x+5,transform.position.y-1.5f,transform.position.z);
+		else
+			g.transform.position = new Vector3 (transform.position.x-5,transform.position.y-1.5f,transform.position.z);
+	}
 	void shootFireball() {
 		GameObject g = (GameObject)Instantiate(Resources.Load ("Fireball"));
 		g.transform.position = new Vector3 (transform.position.x+1,transform.position.y,transform.position.z);
 		Vector3 v = new Vector3 (10, 2, 0);
+		if (transform.eulerAngles.y != 0) {
+			v = new Vector3 (-v.x, v.y, 0);
+			g.transform.position = new Vector3 (transform.position.x-1,transform.position.y,transform.position.z);
+		}
+
 		g.GetComponent<Rigidbody2D> ().velocity = v;
 		if (level == 1)
 			g.GetComponent<FireballScript>().type = "normal";
