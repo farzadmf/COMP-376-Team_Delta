0f4c47588b3fa802ef52761e6e1ad63ff49b30d6
Kim Parise <parise.kim@gmail.com>
2016-10-28 20:54:09 -0400

    Created prototype level - falling wall doesn't work, I have yet to figure it out - fiddled with the camera movement so it doesn't show the two edges of level - there is an area for wall jumping but have no implemented it - Cannot keep player from walking off edge of the map/out of boss room     - I cannot figure out how the character moves so I cannot stop it from moving

---
+++ Assets/Scripts/Utilities/MovingLedgeScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingLedgeScript : MonoBehaviour {
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            player.transform.parent = transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            player.transform.parent = null;
+        }
+    }
+}
