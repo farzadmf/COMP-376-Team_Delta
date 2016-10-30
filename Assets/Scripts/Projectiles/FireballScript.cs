73722c17557f72a30bde851fce8d0883422446a3
Jeremy Bertin <jeremybertin2@gmail.com>
2016-10-30 14:45:34 -0400

    FireBall is now a trigger to prevent enemy knock back. Moved the Damage Popup canvas so that it is out of the way of the level so that you can access level elements in the editor

--- Assets/Scripts/Projectiles/FireballScript.cs
+++ Assets/Scripts/Projectiles/FireballScript.cs
@@ -19,7 +19,28 @@ public class FireballScript : MonoBehaviour {
 		}
 		Destroy (gameObject);
 	}
-	void burnEffect(Collision2D c) {
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        
+        if (c.gameObject.tag == "Enemy")
+        {
+            burnEffect(c);
+        }
+        if (c.gameObject.tag == "Enemy" || c.gameObject.tag == "Ground")
+        {
+            if (type == "exploding")
+            {
+                GameObject g = (GameObject)Instantiate(Resources.Load("Boom"));
+                g.transform.position = transform.position;
+            }
+
+            Destroy(gameObject);
+        }
+        
+    }
+
+    void burnEffect(Collision2D c) {
 		GameObject g = (GameObject)Instantiate (Resources.Load ("Fire1"));
 		Vector3 enemyPos = c.gameObject.transform.position;
 		int random = Random.Range (0,3);
@@ -34,8 +55,26 @@ public class FireballScript : MonoBehaviour {
 		g.transform.SetParent (c.gameObject.transform);
 		g.transform.localPosition = new Vector3 (g.transform.localPosition.x,g.transform.localPosition.y,-1.2f);
 	}
-	// Update is called once per frame
-	void Update () {
+
+    void burnEffect(Collider2D  c)
+    {
+        GameObject g = (GameObject)Instantiate(Resources.Load("Fire1"));
+        Vector3 enemyPos = c.gameObject.transform.position;
+        int random = Random.Range(0, 3);
+        Vector3 newPos;
+        if (random == 0)
+            newPos = new Vector3(enemyPos.x - 0.2f, enemyPos.y - 0.2f, 0);
+        else if (random == 1)
+            newPos = new Vector3(enemyPos.x, enemyPos.y + 0.3f, 0);
+        else
+            newPos = new Vector3(enemyPos.x + 0.2f, enemyPos.y, 0);
+        g.transform.position = newPos;
+        g.transform.SetParent(c.gameObject.transform);
+        g.transform.localPosition = new Vector3(g.transform.localPosition.x, g.transform.localPosition.y, -1.2f);
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
