e92d7e9224e2cea3a36bed68fa8de6ee110fc299
Kim Parise <parise.kim@gmail.com>
2016-10-31 12:53:53 -0400

    Made partner speech now appear in speach bubbles next to partners body - still working on it

--- Assets/Scripts/Player/PartnerScript.cs
+++ Assets/Scripts/Player/PartnerScript.cs
@@ -1,41 +1,81 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 public class PartnerScript : MonoBehaviour {
-	private GameObject text;
+    [SerializeField]
+	private Text text;
+    private Canvas bubble;
+    [SerializeField]
+    private bool once;
+
 	// Use this for initialization
 	void Start () {
-		text = transform.GetChild (0).gameObject;
+        bubble = text.GetComponentInParent<Canvas>();
+
+        bubble.enabled = false;
 	}
 
 	void OnTriggerEnter2D(Collider2D c) {
 		if (c.tag == "Tip") {
 			showMessage(c.gameObject.GetComponent<TipScript>().tipNumber,c.gameObject.GetComponent<TipScript>().activatedOnce
 				,c.gameObject.GetComponent<TipScript>().message,c.gameObject.GetComponent<TipScript>().timeOn);
-			c.GetComponent<TipScript> ().activatedOnce = true;
+			c.GetComponent<TipScript>().activatedOnce = true;
 		}
 	}
 
-	void showMessage(int i, bool activatedOnce, string message, int howLong) {
+	void showMessage(int tipN, bool activatedOnce, string message, int howLong) {
 		if (activatedOnce == false) {
+            setPosition(tipN);
+            bubble.enabled = true;
 			activateText (message, howLong);
 		}
 	}
+
 	void activateText(string message, int howLong) {
 		if (IsInvoking ("deleteText")) {
 			CancelInvoke ("deleteText");
 		}
-		text.GetComponent<TextMesh>().text = message;
+		text.text = message;
 		Quaternion rot = text.transform.rotation;
 		Vector3 playerRot = transform.parent.rotation.eulerAngles;
 		rot.eulerAngles = -playerRot;
 		text.transform.localRotation = rot;
 		Invoke ("deleteText",howLong);
 	}
-	void deleteText() {
-		text.GetComponent<TextMesh> ().text = "";
+
+    void deleteText()
+    {
+        bubble.enabled = false;
+		text.text = "";
 	}
+
+    void setPosition(int tipN)
+    {
+        switch (tipN)
+        {
+            case 1:
+                bubble.transform.localPosition = new Vector3(6.53f, 6.44f, 0.0f);
+                break;
+            case 2:
+                bubble.transform.localPosition = new Vector3(6.53f, 8.5f, 0.0f);
+                break;
+            case 3:
+                break;
+            case 4:
+                bubble.transform.localPosition = new Vector3(6.53f, 8.5f, 0.0f);
+                break;
+            default:
+                break;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+            bubble.enabled = true;
+        if (once)
+        {
+            setPosition(0);
+            once = false;
+        }
 	}
 }
