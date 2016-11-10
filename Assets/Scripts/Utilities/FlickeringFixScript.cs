ad1c1190d73b7fe9052a70b42f7aeafddda22503
Radu <radu.saghin94@gmail.com>
2016-11-10 16:25:14 -0500

    Added flickering fix script to guess fix flickering...

---
+++ Assets/Scripts/Utilities/FlickeringFixScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringFixScript : MonoBehaviour {
+	private float factor;
+
+	void Start () {
+		//if ( factor == 0)
+		factor = 0.005f;
+		Vector3 v = transform.localScale;
+		v.x += factor;
+		v.y += factor;
+		transform.localScale = v;
+
+		float newZ = Random.Range(transform.position.z,transform.position.z+0.2f);
+		Vector3 vv = transform.position;
+		vv.z = newZ;
+		transform.position = vv;
+	}
+
+
+}
