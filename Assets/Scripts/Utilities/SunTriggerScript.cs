39c4be5c7991cf7fc68a973081c69e759c00207e
Radu <radu.saghin94@gmail.com>
2016-11-15 12:06:44 -0500

    Fixed flickering underground, fixed light on the tile where you used to just fall through somehow. Also fixed sun showing underground. It still provides light to player and goblin while underground though.

---
+++ Assets/Scripts/Utilities/SunTriggerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunTriggerScript : MonoBehaviour {
+	public bool sunOn;
+	private GameObject sun;
+	// Use this for initialization
+	void Start () {
+		sun = GameObject.Find ("Sun");
+	}
+	void OnTriggerEnter2D(Collider2D c) {
+		if (!sunOn) {
+			if (c.tag == "Player") {
+				
+			}
+		} else {
+			if (c.tag == "Player") {
+				
+			}
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
