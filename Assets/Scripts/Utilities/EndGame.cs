667b5ba48def5f23669e42801fd3e2b97bcabe37
Jeremy Bertin <jeremybertin2@gmail.com>
2016-11-29 13:40:09 -0500

    Added End Game Credits Scene with music and scrolling text. Triggered by a EndGame prefab in the Events prefab folder.

---
+++ Assets/Scripts/Utilities/EndGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+    public  void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
+    
+}
